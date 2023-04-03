48c11d48c3022f64341085803bc0fccf4872c192
Wilfred Nankpak Bale <nankpakbalewilfred@gmail.com>
2023-04-03 23:50:51 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# displays the size of the body of the response
-curl -s "$1" | wc -c
+
+# Get the URL from the user
+read -p "Enter a URL: " url
+
+# Send a request to the URL and save the response to a temporary file
+response=$(curl -s -w "%{size_download}" -o /dev/null $url)
+
+# Display the size of the response body
+echo "The size of the response body is $response bytes."
